0eaa1d9415700d7aa39a054340e49b00071b4dd5
DANIEL RUBEN RIVAS SALTOS <72212902+rubendaniel9292@users.noreply.github.com>
2023-10-26 16:21:26 -0500

    Metodos de consultas de monggose añadidos

--- src/controller/Articles.Controler.ts
+++ src/controller/Articles.Controler.ts
@@ -1,59 +1,130 @@
 /*CONTROLADOR*/
-import { Request, Response } from 'express';
-import validator from 'validator';
-import Articles from '../model/Articles';
-
+import { Request, Response } from "express";
+import validator from "validator";
+import Articles from "../model/Articles";
+//metodo para probar el controlador (puede comentarse o eliminarse)
 export const prueba = (req: Request, res: Response) => {
-    return res.status(200).json({ message: 'Accion de prueba del controlador exitosa' });
-}
-
+    return res
+        .status(200)
+        .json({ message: "Accion de prueba del controlador exitosa" });
+};
+//metodo para probar el controlador con un objeto (puede comentarse o eliminarse)
 export const curso = (req: Request, res: Response) => {
-    console.log('Se ha ejecutado el endpoint probando');
+    console.log("Se ha ejecutado el endpoint probando");
     return res.status(200).send(
         //la api devuelve dentro del calback un onbjeto json
-        [{
-            curso: 'master en react',
-            autor: 'Daniel Rivas Web',
-            url: ''
-        }]
-
+        [
+            {
+                curso: "master en react",
+                autor: "Daniel Rivas Web",
+                url: "",
+            },
+        ]
     );
-}
+};
 
-//metodo para crear y guarar el articulo
+//Metodo para crear y guarar el articulo
 export const save = async (req: Request, res: Response) => {
     //1: recoger los parametros por post a guardar
     let params = req.body;
-    //2: valdiar los datos datos vacios y longitud 
+    //2: valdiar los datos datos vacios y longitud
     try {
-        let validarTitle = !validator.isEmpty(params.title) && validator.isLength(params.title, { min: 5, max: undefined });
+        let validarTitle =
+            !validator.isEmpty(params.title) &&
+            validator.isLength(params.title, { min: 5, max: undefined });
         let validarContent = !validator.isEmpty(params.content);
         if (!validarTitle || !validarContent) {
-            throw new Error('No se ha validado la informacion');
+            throw new Error("No se ha validado la informacion");
         }
 
         //3: Creando objeto a guardar en la bd
         //asignar valores a objetos al modelo (manual o automatico)
         //article.title = params.title;(manual, para pocos parametros)
-        const article = new Articles(params);//de manera automatica, para muchos paramtos
+        const article = new Articles(params); //de manera automatica, para muchos paramtos
         const articleSaved = await article.save();
         return res.status(200).json({
             status: "success",
             article: articleSaved,
-            message: "Artículo guardado con éxito."
-        })
-
+            message: "Artículo guardado con éxito.",
+        });
     } catch (error) {
         return res.status(400).json({
-            status: 'error', message: 'Faltan datos por enviar o no se ha guardado el artículo'
+            status: "error",
+            message: "Faltan datos por enviar o no se ha guardado el artículo",
         });
     }
+};
+
+//metodo para conseguir el listado de todos articulos
+export const getArticle = (req: Request, res: Response) => {
+    let query = Articles;
+    query.find({})
+        .then((Articles) => {
+            return res.status(200).send({
+                status: "success",
+                Articles,
+            });
+        })
+        .catch((error) => {
+            return res.status(404).json({
+                status: "error",
+                error,
+                message: "No se han encontrado artículos",
+            });
+        });
+};
+
+//metodo para conseguir el listado ordeando de todos articulos con metodos de Mongoose con el campo deseado
+export const getArticleSrot = (req: Request, res: Response) => {
+
+    let query = Articles.find({});
+
+    if (req.params.ultimos) {
+        //valida si el parametro existe y despues si es un numero
+        const limit = parseInt(req.params.ultimos, 10);
+        //lo convierte en base numérica de base 10 que es lo sugerido paa tener el nuero exacto y luego lo valida si es un numero o no
+        if (!isNaN(limit)) {
+            query.limit(limit);
+        } else {
+            return res.status(404).json({ error: `'El parámetro "${req.params.ultimos}" no es un número válido.'` });
+        }
+    }
+
+    //parametro fecha: de mas antiguio a mas reciente con date:1
+    //y de mas reciente a mas antiguio con date:-1
+    query.sort({ date: -1 })
+        .then((Articles) => {
+            return res.status(200).send({
+                status: "success",
+                contador: Articles.length,
+                Articles,
+            });
+        })
+        .catch((error) => {
+            return res.status(404).json({
+                status: "error",
+                error,
+                message: "No se han encontrado artículos",
+            });
+        });
+};
+//metodo para conseguir un solo articulo en la pagina del blog
+export const uno = (req: Request, res: Response) => {
+    //1: recoger id por la url, buscar el articulo con el metodo findById y sino existe devolver un error 
+    let id = req.params.id;
+    Articles.findById(id).then((Articles) => {
+        return res.status(200).json({
+            status: "success",
+            Articles
+        })
+    }).catch((error) => {
+        return res.status(404).json({
+            status: "error",
+            error,
+            message: `No se han encontrado el articulo con id ${id}`,
+        });
+    });
+    
+
 
 }
-
-
-
-
-
-
-
--- src/routes/Articles.Rutes.ts
+++ src/routes/Articles.Rutes.ts
@@ -2,13 +2,25 @@
 import express from 'express';
 const router = express.Router();
 //cargar el controlador
-import { prueba, curso, save } from '../controller/Articles.Controler';
+import { prueba, curso, save, getArticle, getArticleSrot, uno } from '../controller/Articles.Controler';
 
 //rutas de pruebas
 router.get('/ruta-de-prueba', prueba);
-//ruta del curso
+//ruta del curso con objeto
 router.get('/curso', curso);
 
 //ruta para crear y guardar en la bd
 router.post('/crear', save);
+
+//ruta para  conseguir todos los articulos o los ordenados, dependinedo del metodo escogido del controlador y con parametros contador no obligator
+router.get('/articulos/:ultimos?', getArticleSrot);
+
+//ruta para  conseguir todos los articulos
+router.get('/articulos/:ultimos?', getArticle);
+
+//ruta para  conseguir un solo articulo con parametro id obligatorio
+router.get('/articulo/:id', uno);
 export default router;
+
+
+
